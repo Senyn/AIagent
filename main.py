6e317a54d7a6c34e43c2a7a32dda708eea320bdf
Senyn <cxianjie94@gmail.com>
2026-01-17 23:54:09 +0800

    1.5: Boot.dev lesson requirement completion

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ from urllib import response
 from dotenv import load_dotenv
 import argparse
 from google.genai import types
+import call_function
 from prompts import system_prompt
 from functions.get_files_info import schema_get_files_info
 from functions.get_file_content import schema_get_file_content
 from functions.write_files import schema_write_file
 from functions.run_python_file import schema_run_python_file
+
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -29,29 +31,49 @@ available_functions = types.Tool(
     function_declarations=[schema_get_files_info, schema_get_file_content, schema_write_file, schema_run_python_file],
 )
 
-response = client.models.generate_content(
-    model='gemini-2.5-flash',
-    contents=messages,
-    config=types.GenerateContentConfig(
+
+    
+
+def main():
+    for _ in range(20):  # Limit to 20 iterations to prevent infinite loops
+
+        # Generate content with function calling
+        response = client.models.generate_content(
+        model='gemini-2.5-flash',
+        contents=messages,
+        config=types.GenerateContentConfig(
         tools=[available_functions], 
         system_instruction=system_prompt, 
         temperature=0)
-    )
+        )
 
-def main():
+        if response.candidates is not None:
+            for candidate in response.candidates:
+                messages.append(candidate.content)
 
-    if response.function_calls is not None:
-        for f_call in response.function_calls:    
-            print(f"Calling function: {f_call.name}({f_call.args})")
-    else:
-        if response.usage_metadata is not None:             
-            if args.verbose:
-                print(f"User prompt: {args.user_prompt}")
-                print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-                print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-            print(f"Response:\n{response.text}")
+        if response.function_calls is not None:
+            function_call_list = []
+            for function_call in response.function_calls:
+                function_call_result = call_function.call_function(function_call, args)
+                if function_call_result.parts[0].function_response.response is not None:
+                    function_call_list.append(function_call_result.parts[0])
+                    print(f"Function call result:\n{function_call_result.parts[0].function_response.response['result']}")                            
+                else:
+                    print("Function call resulted in no response.") 
+                    break
+                messages.append(types.Content(role="user", parts=function_call_list))         
         else:
-            raise RuntimeError("No usage metadata found in response")
+            if response.usage_metadata is not None:             
+                if args.verbose:
+                    print(f"User prompt: {args.user_prompt}")
+                    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+                    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+                print(f"Response:\n{response.text}")
+                break
+            else:
+                raise RuntimeError("No usage metadata found in response")
+            
+        
 
 
 if __name__ == "__main__":
